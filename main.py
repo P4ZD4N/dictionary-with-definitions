34827bde38df6eeae8d49cbbfa487caf9dd38295
Wiktor <wiko700@gmail.com>
2022-12-14 23:14:40 +0100

    Fix Fix of bug. The bug was that the program was case sensitive, which made complications.

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 dictionary = {}
 
 while True:
@@ -12,27 +11,27 @@ while True:
     user_option = int(input("Enter option: "))
 
     if user_option == 1:
-        word = input("What word do you want to define?: ")
+        word = input("What word do you want to define?: ").lower()
         if word in dictionary:
             print(f"Dictionary actually contain a definition of {word}!")
             continue
         else:
-            definition = input("Now enter definition of this word: ")
+            definition = input("Now enter definition of this word: ").lower()
             dictionary.update({word: definition})
             print(f"{word} definition was successfully added to dictionary!")
             continue
 
     elif user_option == 2:
-        def_to_find = input("Enter word for which you want to find definition: ")
+        def_to_find = input("Enter word for which you want to find definition: ").lower()
         if def_to_find in dictionary:
-            print(f"{def_to_find} - {dictionary[def_to_find]}")
+            print(f"{def_to_find.capitalize()} - {dictionary[def_to_find].capitalize()}")
             continue
         else:
             print(f"Dictionary doesn't contain a definition of {def_to_find}!")
             continue
 
     elif user_option == 3:
-        def_to_del = input("Enter word which definition you want to delete: ")
+        def_to_del = input("Enter word which definition you want to delete: ").lower()
         if def_to_del in dictionary:
             dictionary.pop(def_to_del)
             print(f"{def_to_del} definition was successfully deleted from dictionary!")
